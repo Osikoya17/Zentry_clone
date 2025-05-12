5869ca99fcd3778d45eca1133c85f1d0ffb92a5b
Olaoluwa David Osikoya <osikoyaolaoluwa2021@gmail.com>
2025-05-12 19:17:48 +0100

    Fix Bugs and Errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from "./components/About.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Features from "./components/Features.jsx";
 import Story from "./components/Story.jsx";
+import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
@@ -12,6 +14,8 @@ const App = () => {
             <About/>
             <Features/>
             <Story/>
+            <Contact/>
+            <Footer/>
         </main>
     )
 }
